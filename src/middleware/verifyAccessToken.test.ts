929f803780f93374a38f00125d3d19be98af6537
Brad Treloar <brad.treloar@pm.me>
2022-03-06 19:28:44 +1030

    add verifyAccessToken middleware

---
+++ src/middleware/verifyAccessToken.test.ts
@@ -0,0 +1,118 @@
+import { generateAccessToken } from "@app/auth";
+import initSequelize from "@app/database";
+import { UserFactory } from "@app/factories/User";
+import verifyAccessTokenMiddleware from "@app/middleware/verifyAccessToken";
+import User from "@app/models/User";
+import faker from "@faker-js/faker";
+import { Request, Response } from "express";
+
+describe("verifyAccessToken middleware", () => {
+  it("calls next middleware when token is valid", async () => {
+    await initSequelize(":memory:");
+    const secret = faker.random.alphaNumeric(20);
+    await UserFactory.create();
+    const users = await User.findAll();
+    const user = users.shift() as User;
+    const token = generateAccessToken(user, secret);
+    const encodedToken = Buffer.from(token).toString("base64");
+    const req = {
+      headers: {
+        authorization: `Bearer ${encodedToken}`,
+      },
+    } as Request;
+    const next = jest.fn();
+    verifyAccessTokenMiddleware(secret)(req, {} as Response, next);
+    expect(next).toHaveBeenCalled();
+  });
+
+  it("decorates request with user payload", async () => {
+    await initSequelize(":memory:");
+    const secret = faker.random.alphaNumeric(20);
+    await UserFactory.create();
+    const users = await User.findAll();
+    const user = users.shift() as User;
+    const token = generateAccessToken(user, secret);
+    const encodedToken = Buffer.from(token).toString("base64");
+    const req = {
+      headers: {
+        authorization: `Bearer ${encodedToken}`,
+      },
+    } as Request;
+    const next = jest.fn();
+    verifyAccessTokenMiddleware(secret)(req, {} as Response, next);
+    expect(next).toHaveBeenCalled();
+    expect(req.user).toStrictEqual({
+      id: user.id,
+      email: user.email,
+    });
+  });
+
+  it("sets 401 response and error when token not found", async () => {
+    const secret = faker.random.alphaNumeric(20);
+    const req = {
+      headers: {},
+    } as Request;
+    const mockResponseJson = jest.fn();
+    const mockResponseStatus = jest.fn().mockReturnValue({
+      json: mockResponseJson,
+    });
+    const res = {
+      status: mockResponseStatus,
+    } as unknown as Response;
+    const next = jest.fn();
+    verifyAccessTokenMiddleware(secret)(req, res, next);
+    expect(next).not.toHaveBeenCalled();
+    expect(mockResponseStatus).toHaveBeenCalledWith(401);
+    expect(mockResponseJson).toHaveBeenCalledWith({
+      error: "Access token not found",
+    });
+  });
+
+  it("sets 401 response and error when authZ header is invalid", async () => {
+    const secret = faker.random.alphaNumeric(20);
+    const req = {
+      headers: {
+        authorization: `${faker.random.alphaNumeric(20)}`,
+      },
+    } as Request;
+    const mockResponseJson = jest.fn();
+    const mockResponseStatus = jest.fn().mockReturnValue({
+      json: mockResponseJson,
+    });
+    const res = {
+      status: mockResponseStatus,
+    } as unknown as Response;
+    const next = jest.fn();
+    verifyAccessTokenMiddleware(secret)(req, res, next);
+    expect(next).not.toHaveBeenCalled();
+    expect(mockResponseStatus).toHaveBeenCalledWith(401);
+    expect(mockResponseJson).toHaveBeenCalledWith({
+      error: "Invalid access token",
+    });
+  });
+
+  it("sets 401 response and error when token is invalid", async () => {
+    const secret = faker.random.alphaNumeric(20);
+    const token = faker.random.alphaNumeric(20);
+    const encodedToken = Buffer.from(token).toString("base64");
+    const req = {
+      headers: {
+        authorization: `Bearer ${encodedToken}`,
+      },
+    } as Request;
+    const mockResponseJson = jest.fn();
+    const mockResponseStatus = jest.fn().mockReturnValue({
+      json: mockResponseJson,
+    });
+    const res = {
+      status: mockResponseStatus,
+    } as unknown as Response;
+    const next = jest.fn();
+    verifyAccessTokenMiddleware(secret)(req, res, next);
+    expect(next).not.toHaveBeenCalled();
+    expect(mockResponseStatus).toHaveBeenCalledWith(401);
+    expect(mockResponseJson).toHaveBeenCalledWith({
+      error: "Could not verify access token",
+    });
+  });
+});
---
+++ src/middleware/verifyAccessToken.ts
@@ -0,0 +1,37 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import { Middleware } from "./types";
+import { verifyAccessToken } from "@app/auth/index";
+import { UserPayload } from "@app/auth/types";
+
+const verifyAccessTokenMiddleware =
+  (secret: string): Middleware =>
+  (req, res, next) => {
+    const bearerToken = req.headers["authorization"];
+    if (bearerToken !== undefined) {
+      const [type, encodedToken] = bearerToken.split(" ");
+      if (type !== "Bearer") {
+        res.status(401).json({
+          error: "Invalid access token",
+        });
+      } else {
+        const token = Buffer.from(encodedToken, "base64").toString();
+        try {
+          const payload = verifyAccessToken(token, secret);
+          req.user = payload.user as UserPayload;
+          next();
+        } catch (error) {
+          if (error instanceof JsonWebTokenError) {
+            res.status(401).json({
+              error: "Could not verify access token",
+            });
+          }
+        }
+      }
+    } else {
+      res.status(401).json({
+        error: "Access token not found",
+      });
+    }
+  };
+
+export default verifyAccessTokenMiddleware;
